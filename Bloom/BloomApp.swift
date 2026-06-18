59fd5f3f1d274faeb0637ee2a216d9b9aa9a1234
dikiyp <hopabsq@mail.ru>
2026-06-19 00:36:50 +0600

    Init Bloom-iOS

---
+++ Bloom/BloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomApp.swift
+//  Bloom
+//
+//  Created by Аскольд on 19.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct BloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
