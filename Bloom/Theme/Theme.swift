7c3824530469f6c759fbdc983bde1ed5636c3f68
dikiyp <hopabsq@mail.ru>
2026-07-05 18:41:49 +0600

    Change glass tint color

--- Bloom/Theme/Theme.swift
+++ Bloom/Theme/Theme.swift
@@ -83,7 +83,7 @@ struct Theme {
         static let grayBackground = Color.dynamic(lightHex: "f1f1f4", darkHex: "000000ff")
         static let secondaryText = Color.dynamic(lightHex: "0000006b", darkHex: "ffffff6b")
         static let switcher = Color.dynamic(lightHex: "d7d7db", darkHex: "595959")
-        static let pressable = Color.dynamic(lightHex: "a6a6a6", darkHex: "333333") 
+        static let pressable = Color.dynamic(lightHex: "d2d0d8", darkHex: "333333") 
         static let sectionForeground = Color.dynamic(lightHex: "ffffff", darkHex: "1b1b1d")
         static let foreground = Color.dynamic(lightHex: "f1f1f4", darkHex: "1b1b1d")
         static let foregroundTransparent = Color.dynamic(lightHex: "0000001c", darkHex: "ffffff1c")
